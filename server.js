b917061ac24c0dfb02ac537b131c1d213b0ef881
Haidil-SMU <haidil.ms.2025@computing.smu.edu.sg>
2026-03-05 11:37:15 +0800

    added server code

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const server = express();
+const path = require("path");
+
+server.use("/", express.static(path.join(__dirname, "public")))
+server.use(express.urlencoded({ extended: true }));
+
+//Start of Express Router Code
+
+
+
+//End of Express Router Code
+
+const hostname = "127.0.0.1";
+const port = 8000;
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
