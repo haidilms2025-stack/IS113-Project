55af9ced643f1d40ff93dc5d1a8077a60f7d9011
Ashrel <122988943+Ashrel@users.noreply.github.com>
2026-03-15 16:27:06 +0800

    combine server.js ash

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const server = express();
 server.use(express.urlencoded({ extended: true }));
 server.set("view engine", "ejs");
 
-const recipesRoute = require("./routes/recipeRoute.js")
+const authRoutes = require("./routes/ashrel_auth") //ash route
+const recipesRoute = require("./routes/recipeRoute.js") //hadi route
+
+server.use("/", authRoutes);        // handles /login, /register ash part
 server.use('/recipes', recipesRoute) //any path that starts with recipe, we wil send it to this route 
 
 // END OF YOUR CODE HERE
